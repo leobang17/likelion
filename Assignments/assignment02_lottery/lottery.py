12ef56f15bfc9994ccae861069ef2ab831e8c0d9
Leo Bang <bluke8489@gmail.com>
2021-04-11 18:45:24 +0900

    lottery assignment final

--- Assignments/assignment02_lottery/lottery.py
+++ Assignments/assignment02_lottery/lottery.py
@@ -1,48 +1,71 @@
 import random
 
 MSG_PURCHASE = "> 구입 금액을 입력해주세요: "
-MSG_DTYPE_ERROR = "> 숫자를 입력해주세요!"
+MSG_VALUE_ERROR = "> 숫자만 입력해주세요!"
 MSG_RANGE_ERROR = "> 1 ~ 45 까지의 숫자를 입력하세요!"
+MSG_INAPPROPRIATE_NUMBERS_OF_INPUT = "> 6개의 숫자를 입력하세요."
+MSG_INPUT_OVERLAPPED = "> 중복된 숫자가 존재합니다!"
 MSG_INPUT_LOT_NUM = "> 지난 주 당첨 번호를 입력해주세요: "
 MSG_RESULT = "> 로또 당첨 결과"
 
+def main():
+    run()
 
+# 얼마 구입할 건지 입력
 def input_purchase_price():
     purchase_price = input(MSG_PURCHASE)
     while(purchase_price.isdigit() == False) :
-        print(MSG_DTYPE_ERROR)
+        print(MSG_VALUE_ERROR)
         purchase_price = input(MSG_PURCHASE)
 
     return purchase_price
 
+# 당첨 번호 입력
 def input_lotteryNum():    
-    # input 범위 지정.
-    lotteryNum = [] 
-    for i in range(6):
-        temp = input(MSG_INPUT_LOT_NUM)
-        while(temp.isdigit() == False):
-            print(MSG_RANGE_ERROR)
-            temp = input(MSG_INPUT_LOT_NUM)
-        while (int(temp) not in range(1, 46)):
-            print(MSG_RANGE_ERROR)
-            temp = input(MSG_INPUT_LOT_NUM)
-        lotteryNum.append(temp)
-    # 기존에 뽑은 번호는 못 뽑게 제한할 것. 
-    lotteryNum.sort()
+    lotteryNum = []
+
+    while True:
+        count = 0
+        lotteryNum = []
+        try: 
+            temp = input(MSG_INPUT_LOT_NUM).split(',')
+
+            if len(temp) != 6:
+                print(MSG_INAPPROPRIATE_NUMBERS_OF_INPUT)
+                continue
+            for i in range(6):
+                if int(temp[i]) not in range(1, 46):
+                    print(MSG_RANGE_ERROR)
+                    break
+                elif int(temp[i]) in lotteryNum:
+                    print(MSG_INPUT_OVERLAPPED) 
+                    break
+                else: 
+                    lotteryNum.append(int(temp[i]))
+                    count += 1
+            if count == 6:
+                break
+        except ValueError:
+            print(MSG_VALUE_ERROR)
+            continue
+    lotteryNum.sort()  
+    return(lotteryNum)
     
-    return lotteryNum
-    
+# 몇 장 구입했는지 계산
 def compute_purchase_amount(purchase_price):
     purchase_amount = int(purchase_price) / 1000
     return int(purchase_amount)
 
+# 몇 장 구매했는지 출력
 def print_purchase_amount(purchase_amount):
     print("{}장의 로또를 구입하셨습니다.".format(purchase_amount))
 
+# 구매한 로또 번호 출력
 def print_lottery(lotteryArr):
     for i in range(len(lotteryArr)):
         print(lotteryArr[i])
 
+# 로또 결과 출력
 def print_result(count_first, count_second, count_third, count_fourth):
     print(MSG_RESULT)
     print("4등 - 5000원 - {}개".format(count_fourth))
@@ -50,11 +73,17 @@ def print_result(count_first, count_second, count_third, count_fourth):
     print("2등 - 100000원 - {}개".format(count_second))
     print("1등 - 5000000원 - {}개".format(count_first))
 
-def print_yield(count_first, count_second, count_third, count_fourth, purchase_price):
-    lottery_yield = (5000 * count_fourth + 20000 * count_third + 100000 * count_second + 5000000 * count_win) / purchase_price
+# 로또 결과 출력2 (구매금액, 당첨금액, 거스름돈, 수익률)
+def print_yield(count_first, count_second, count_third, count_fourth, purchase_price, purchase_amount):
+    lottery_prize = (5000 * count_fourth + 20000 * count_third + 100000 * count_second + 5000000 * count_first)
+    purchase_change = int(purchase_price) % 1000
+    lottery_yield =  lottery_prize / (int(purchase_price) - purchase_change)
+
+    print("> 구매금액: {}원\n> 당첨금액: {}원\n> 거스름돈: {}원".format(purchase_price, lottery_prize, purchase_change)) 
     print("> 수익률")
     print(lottery_yield, "배")
-
+    
+# 구매 매수 만큼 로또 번호 추첨. 
 def run_lottery(purchase_amount):
     lotteryArr = []
     for i in range(purchase_amount):
@@ -63,9 +92,9 @@ def run_lottery(purchase_amount):
             
     return lotteryArr
 
+# 로또 결과 확인
 def count_win(lotteryArr, lotteryNum, purchase_amount):
-    count_first, count_second, count_third, count_fourth = 0, 0, 0, 0
-    
+    count_first, count_second, count_third, count_fourth = 0, 0, 0, 0    
     for i in range(purchase_amount):
         win_count = 0
 
@@ -82,32 +111,29 @@ def count_win(lotteryArr, lotteryNum, purchase_amount):
         elif win_count == 3:
             count_fourth += 1
             
-    return(count_first, count_second, count_third, count_fourth)
+    return [count_first, count_second, count_third, count_fourth]
 
-
-
+# run 함수
 def run():
     purchase_price = input_purchase_price()
     purchase_amount = compute_purchase_amount(purchase_price)
 
     print_purchase_amount(purchase_amount)
 
-    # 구입한 금액에 비례해서 lottery를 생성
     lotteryArr = run_lottery(purchase_amount)
 
     print_lottery(lotteryArr)
+    print("\n")
 
-    # user가 입력한 lottery 번호 
     lotteryNum = input_lotteryNum()
-    print(lotteryNum)
+    print(lotteryNum, "\n")
 
     count_first, count_second, count_third, count_fourth = count_win(lotteryArr, lotteryNum, purchase_amount)
-    
-    print(count_first, count_second, count_third, count_fourth)
-    
 
-run()
+    print_result(count_first, count_second, count_third, count_fourth)
+    print("\n")
 
-# lotteryNum = input_lotteryNum()
-# print(lotteryNum)
-# print(type(lotteryNum))
+    print_yield(count_first, count_second, count_third, count_fourth, purchase_price, purchase_amount)
+
+if __name__ == "__main__" :
+    main()
